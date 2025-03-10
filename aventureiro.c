f63fd6b4550b88dffafbf97dd1b23280ea861c39
programacaojhon <robertalelis123@gmail.com>
2025-03-10 21:56:01 +0000

    aventureiro1.c

---
+++ aventureiro.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+float nota1, nota2, nota3;
+float media;
+
+printf(" *** Programa de Cálculo de Média *** \n");
+
+printf("Digite a sua primeira nota: \n");
+scanf("%f", &nota1);
+
+printf("Digite a sua segunda nota: \n");
+scanf("%f", &nota2);
+
+printf("Digite a sua terceira nota: \n");
+scanf("%f", &nota3);
+
+media = (nota1 + nota2 +nota3) / 3;
+
+printf("A sua média é: \n %.2f", media);
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+}
